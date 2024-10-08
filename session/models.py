27af1b6a44303a257ba86147d6cef4223866f770
TannerBates <tanner_bts@yahoo.com>
2024-10-08 11:57:13 -0500

    connecting post model to abstract user model

--- session/models.py
+++ session/models.py
@@ -8,3 +8,13 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
