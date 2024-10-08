f07b4b251a326c5d0ccba646a04328a0a7c7f48f
TannerBates <tanner_bts@yahoo.com>
2024-10-08 12:30:27 -0500

    Models changes

--- session/admin.py
+++ session/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm, PostCreateForm
+from .models import CustomUser, Post
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
@@ -10,4 +10,6 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['email', 'username',]
 
+
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Post)
--- session/models.py
+++ session/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django import forms
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 class CustomUser(AbstractUser):
@@ -14,7 +15,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
         return self.title
